0ea95d5a8809425477ea92add6ec7a55bc010916
Oleksandr Solianyk <salym3k@gmail.com>
2022-08-28 14:39:00 +0300

    Add solution for task (Opposite number) from CodeWars.com.

---
+++ 8 kyu/script_oppositeNumber.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function opposite(number) {
+    return number * -1
+}
+
+console.log(opposite(-8));
+
+// Very simple, given an integer or a floating-point number, find its opposite.
