986ffd5255e93d4401b512e42b181b51d67f3c14
Oleksandr Solianyk <salym3k@gmail.com>
2022-08-28 15:12:28 +0300

    Add solution for task (MakeUpperCase) from CodeWars.com.

---
+++ 8 kyu/script_MakeUpperCase.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const makeUpperCase = (str) => str.toUpperCase()
+
+const str = 'hello world'
+console.log(makeUpperCase(str));
+
+// Write a function which converts the input string to uppercase.
