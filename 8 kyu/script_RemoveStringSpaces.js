ef1d98361e11ed70a7ebb2bb923eb6f865cd83da
Oleksandr Solianyk <salym3k@gmail.com>
2022-08-28 20:43:32 +0300

    Add solution for task (Remove String Spaces) from CodeWars.com.

---
+++ 8 kyu/script_RemoveStringSpaces.js
@@ -0,0 +1,7 @@
+'use strict'
+
+// Simple, remove the spaces from the string, then return the resultant string.
+
+const noSpace = x => x.split(' ').join('');
+const str = ('8aaaaa dddd r     ')
+noSpace(str)
