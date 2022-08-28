a0dab89d899839a5d3641c86e488c7cdb0eef30b
Oleksandr Solianyk <salym3k@gmail.com>
2022-08-28 17:36:14 +0300

    Add solution for task (Beginner Series #2 Clock) from CodeWars.com.

--- 8 kyu/script_BeginnerSeriesClock2.js
+++ 8 kyu/script_BeginnerSeriesClock2.js
@@ -1,6 +1,7 @@
 'use strict'
 
-const past = (h = 0, m = 0, s = 0) => h * 3600000 + m * 60000 + s * 1000
+const past = (h, m = 0, s = 0) => h * 3600000 + m * 60000 + s * 1000
 
+console.log(past(1));
 //Clock shows h hours, m minutes and s seconds after midnight.
 //Your task is to write a function which returns the time since midnight in milliseconds.
