32d729604992f634c22b413d999bb49bab8b2193
Oleksandr Solianyk <salym3k@gmail.com>
2022-08-29 17:48:29 +0300

    Add solution for task (Even or Odd) from CodeWars.com.

---
+++ 8 kyu/script_ EvenOrOdd.js
@@ -0,0 +1,8 @@
+'use strict'
+
+// Create a function that takes an integer as an argument and returns
+// "Even" for even numbers or "Odd" for odd numbers.
+
+const even_or_odd = (number) => number % 2 ? 'Odd' : 'Even';
+
+console.log(even_or_odd(5));
