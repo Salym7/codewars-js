7d235659d91b86655a4e599fda090d89144bb08b
Oleksandr Solianyk <salym3k@gmail.com>
2022-08-29 23:51:43 +0300

    Add solution for task (Calculate BMI) from CodeWars.com.

---
+++ 8 kyu/Calculate BMI.js
@@ -0,0 +1,18 @@
+'use strict'
+
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+// if bmi <= 25.0 return "Normal"
+// if bmi <= 30.0 return "Overweight"
+// if bmi > 30 return "Obese"
+
+const bmi = (weight, height) => {
+    const index = weight / height ** 2
+    if (index <= 18.5) return "Underweight"
+    if (index <= 25) return "Normal"
+    if (index <= 30) return "Overweight"
+    return 'Obese'
+}
+
+console.log(bmi(80, 1.80));
