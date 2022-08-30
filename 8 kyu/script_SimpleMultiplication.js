dbd2a9a9b0b4d9d8ab1d5d8b769e48b3e989d082
Oleksandr Solianyk <salym3k@gmail.com>
2022-08-30 20:48:07 +0300

    Add solution for task (Simple multiplication) from CodeWars.com.

---
+++ 8 kyu/script_SimpleMultiplication.js
@@ -0,0 +1,8 @@
+'use strict'
+
+// This kata is about multiplying a given number by eight if it is an even
+// number and by nine otherwise.
+
+const simpleMultiplication = (number) => number % 2 ? number * 9 : number * 8;
+
+console.log(simpleMultiplication(8));
