e9aff2264c2e53aa6a3a077f3be98d9da0ffe7ff
Oleksandr Solianyk <salym3k@gmail.com>
2022-08-30 23:02:49 +0300

    Add solution for task (Unique in order) from CodeWars.com.

--- 6 kyu/script_UniqueInOrder.js
+++ 6 kyu/script_UniqueInOrder.js
@@ -7,7 +7,7 @@
 const uniqueInOrder = (iterable) => {
     const result = [];
     for (let i = 0; i < iterable.length; i++) {
-        if (iterable[i] != iterable[i + 1]) result.push(iterable[i]);
+        if (iterable[i] !== iterable[i + 1]) result.push(iterable[i]);
     }
     return result;
 }
