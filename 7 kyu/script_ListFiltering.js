935ef00b1da3ee7cdb65c59c93626ff05b618631
Oleksandr Solianyk <salym3k@gmail.com>
2022-09-01 21:16:53 +0300

    Add solution for task (List Filtering) from CodeWars.com.

---
+++ 7 kyu/script_ListFiltering.js
@@ -0,0 +1,7 @@
+'use strict'
+
+// In this kata you will create a function that takes a list of non-negative
+// integers and strings and returns a new list with the strings filtered out.
+
+const filter_list = l => l.filter(num =>typeof num === 'number')
+
