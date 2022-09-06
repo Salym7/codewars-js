b60ae23bfa83d14951ac5af5cc1a4525244e63b7
Oleksandr Solianyk <salym3k@gmail.com>
2022-09-06 14:51:22 +0300

    Add solution for task (Invert values) from CodeWars.com.

---
+++ 8 kyu/script_InvertValues.js
@@ -0,0 +1,6 @@
+'use strict'
+
+// Given a set of numbers, return the additive inverse of each. Each
+// positive becomes negatives, and the negatives become positives.
+
+const invert = array => array.map(num => -num)
