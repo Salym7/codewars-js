49e9d3e54f02e806d66ac46cb930aef6bcd9a5a0
Oleksandr Solianyk <salym3k@gmail.com>
2022-09-11 19:10:05 +0300

    Add solution for task (Two to One) from CodeWars.com.

---
+++ 7 kyu/script_Two to One.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// Take 2 strings s1 and s2 including only letters from a to z.
+// Return a new sorted string, the longest possible, containing distinct
+// letters - each taken only once - coming from s1 or s2.
+
+// const longest = (s1 , s2) => {
+//   let str = (s1 + s2).sort()
+//     console.log(str);
+// }
+const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('')
+
+console.log(longest("aretheyhere", "yestheyarehere"));
