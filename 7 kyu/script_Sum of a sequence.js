e26645fa9f2669e84557f8deb3044629653189e5
Oleksandr Solianyk <salym3k@gmail.com>
2022-09-18 17:10:37 +0300

    Add solution for task (Sum of sequence) from CodeWars.com.

---
+++ 7 kyu/script_Sum of a sequence.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// Your task is to make function, which returns the sum of a sequence of integers.
+//
+// The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
+//
+// If begin value is greater than the end, function should returns 0
+
+const sequenceSum = (begin, end, step) => {
+    let result = 0
+    for (let i = begin; i <= end; i += step) {
+        result += i
+    }
+    return result
+};
