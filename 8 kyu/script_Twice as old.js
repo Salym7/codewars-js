407f818f103764b6c0391185a29acc09ce85bacf
Oleksandr Solianyk <salym3k@gmail.com>
2022-09-19 19:33:16 +0300

    Add solution for task (Twice as old) from CodeWars.com.

--- 8 kyu/script_Twice as old.js
+++ 8 kyu/script_Twice as old.js
@@ -1,5 +1,8 @@
 'use strict'
 
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+// The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+
 const twiceAsOld = (dadYearsOld, sonYearsOld) => Math.abs(dadYearsOld - 2 * sonYearsOld);
 
 console.log(twiceAsOld(29, 0));
