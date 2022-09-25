294fa3e710f181de880029c30a4913f78707f56f
Oleksandr Solianyk <salym3k@gmail.com>
2022-09-25 11:17:08 +0300

    Add solution for task (Vowel remover) from CodeWars.com.

---
+++ 8 kyu/script_Vowel remover.js
@@ -0,0 +1,6 @@
+'use strict'
+
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+const shortcut = (string) => string.replace(/([aeiou])/g, '');
+
