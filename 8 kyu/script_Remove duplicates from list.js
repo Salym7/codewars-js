e7cf7e0b6d21085b3ca3d10baaf7c6239f081310
Oleksandr Solianyk <salym3k@gmail.com>
2022-09-30 22:19:26 +0300

    Add solution for task (Remove duplicates from list) from CodeWars.com.

---
+++ 8 kyu/script_Remove duplicates from list.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// Define a function that removes duplicates from an array of numbers and returns it as a result.
+//
+// The order of the sequence has to stay the same.
+
+const distinct = (a) => ([...new Set(a)]);
+
+console.log(distinct([1, 1, 1, 2, 2, 2, 2]));
