a3590d70014dd3731ee73ebd064e7eda84dcde69
Oleksandr Solianyk <salym3k@gmail.com>
2022-10-19 22:27:17 +0300

    Add solution for task (Area of a Square) from CodeWars.com.

---
+++ 8 kyu/script_Area of a Square.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// Complete the function that calculates the area of the red square,
+// when the length of the circular arc A is given as the input.
+// Return the result rounded to two decimals.
+
+const squareArea = (A) => +Math.pow(2 * A / Math.PI, 2).toFixed(2)
+
+console.log(squareArea(2));
