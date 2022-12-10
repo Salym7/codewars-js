db2a77b7342a9767afcea18a649e56150aad1ac9
Oleksandr Solianyk <salym3k@gmail.com>
2022-12-10 21:40:38 +0200

    Add solution for task (Highest Rank Number in an Array) from CodeWars.com.

---
+++ 6 kyu/script_Highest Rank Number in an Array.js
@@ -0,0 +1,22 @@
+// Complete the method which returns the number which is most frequent in the given input array.
+// If there is a tie for most frequent number, return the largest number among them.
+//
+// Note: no empty arrays will be given.
+// Examples
+//
+// [12, 10, 8, 12, 7, 6, 4, 10, 12]              -->  12
+// [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
+// [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
+
+function highestRank(arr) {
+    const obj = {}
+    arr.forEach(x => {
+         (x in obj) ? obj[x] = obj[x] + 1 : obj[x] = 1
+    })
+    const array = Object.entries(obj);
+     array.sort((a ,b)=> b[0] - a[0]).sort((a ,b)=> b[1] - a[1])
+
+    return +array[0][0]
+}
+
+console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12, 10]));
